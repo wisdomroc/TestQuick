1f266c6f93bff23cf1c226134aad8edceef19917
wisdomroc <r.o.c.wang@163.com>
2020-08-24 17:21:57 +0800

    some details

--- Src/main.cpp
+++ Src/main.cpp
@@ -17,40 +17,39 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
-    app.setOrganizationName("jaredTao");
-    app.setOrganizationDomain("https://jaredtao.github.io");
-    Logger::initLog();
+    app.setOrganizationName("CEPRI");
+    app.setApplicationName("TestQuick");
+    app.setOrganizationDomain("https://github.com/wisdomroc/TestQuick");
 
     QQmlApplicationEngine *engine = new QQmlApplicationEngine();
 
-    //向qml注册类型
+    Logger::initLog();
+
+    //! 向qml注册类型
     qmlRegisterType<ReaderModel>("Backend", 1, 0, "ReaderModel");
     qmlRegisterType<ReaderTableModel>("ReaderTableModel", 1, 0, "ReaderTableModel");
-
     qmlRegisterType<FileIO>("Tools", 1, 0, "FileIO");
     qmlRegisterType<FileInfo>("Tools", 1, 0, "FileInfo");
     qmlRegisterType<OperationRecorder>("Tools", 1, 0, "OperationRecorder");
 
-
-
+    //! 向qml传递变量
+    TableStatus tableStatus;
     ReaderModel *readerModel = new ReaderModel();
     ReaderTableModel *readerTableModel = new ReaderTableModel();
-
-    TableStatus tableStatus;
-    //向qml传递变量
     engine->rootContext()->setContextProperty("TableStatus", &tableStatus);
     engine->rootContext()->setContextProperty("readerModel", readerModel);
     engine->rootContext()->setContextProperty("readerTableModel", readerTableModel);
     engine->load(QUrl("qrc:/Qml/main.qml"));
 
-
+    //! 自定义字体
+    /*
     int fontId = QFontDatabase::addApplicationFont(":/Font/fontawesome-webfont.ttf");
     QStringList fontFamilies = QFontDatabase::applicationFontFamilies(fontId);
-    qDebug() << "fontFamilies: " << fontFamilies;
     QFont font;
     font.setFamily(fontFamilies.first());
     font.setPointSize(20);
-    //    app.setFont(font);
+    app.setFont(font);
+    */
 
     return app.exec();
 }
